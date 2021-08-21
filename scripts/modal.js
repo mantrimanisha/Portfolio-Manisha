df793244fd321fecfa5387610d3c9f48ddb7634c
mantrimanisha <59051860+mantrimanisha@users.noreply.github.com>
2021-08-21 19:04:30 +0530

    Portfolio updated

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,45 +1,40 @@
 $(document).ready(function() {
     // MODAL
     var modalText = {
-      discover: {
-        title: 'ChowNow Discover',
-        tag: 'FOOD ORDERING PLATFORM.',
+      todoapp: {
+        title: 'To do App',
+        tag: 'TO DO LIST',
         detail:
-          ' Discover is a platform that lets customers discover new local restaurants and provides business owners with tools to convert first time orders into lifelong diners.',
+          'To do list app is used add and organize tasks fast. Also we can remove task or can update it easily.',
+        link: 'https://manisha-to-do-app-react-05-06-2021.netlify.app/'
+      },
+      expensetracker: {
+        title: 'ExpenseTracker',
+        tag: 'EXPENSE CALCULATOR.',
+        detail:
+          'Expense Tracker offers transaction management, transaction tracking, and transaction history to companies for internal use. A Javascript companion site for the Expense Tracker App.',
+          link: 'https://expense-tracker-ee4540.netlify.app/'
+      },
+      burgerbar: {
+        title: 'Burger Bar',
+        tag: 'BURGER ORDERING PLATFORM.',
+        detail:
+          ' Burger Bar is a platform where customer discover new varieties of burger.',
         link: '#'
       },
-      ordering: {
-        title: 'ChowNow Ordering Web',
-        tag: 'WHITE-LABEL ONLINE ORDERING SOLUTION.',
+      weatherforecast: {
+        title: 'Weather Forecast',
+        tag: 'Weather Prediction.',
         detail:
-          'ChowNow is a commission-free online ordering system and food ordering app helping restaurants feed their hungry customers.',
-        link: 'https://direct.chownow.com/direct/195/locations/260'
+          ' Weather Forecasting provides weather for different cities which we want to find we can search.Hosted site written in Html,css and Javascript.',
+        link: 'https://weather-forecasting-mantri-71a841.netlify.app/'
       },
-      newrelic: {
-        title: 'NewRelic.com',
-        tag: 'APPLICATION PERFORMANCE MONITORING.',
+      rockpaperscissor: {
+        title: 'Rock Paper Scissor',
+        tag: 'ROCK PAPER SCISSOR',
         detail:
-          'Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.',
-        link: 'http://www.newrelic.com'
-      },
-      roambi: {
-        title: 'Roambi.com',
-        tag: 'BUSINESS ANALYTICS.',
-        detail:
-          'Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.',
-        link: 'http://www.roambi.com'
-      },
-      walker: {
-        title: 'WalkerTracker',
-        tag: 'PERFORMANCE METRICS.',
-        detail:
-          'Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.'
-      },
-      mystand: {
-        title: 'MyStand',
-        tag: 'CROWD-FUNDED CHARITY.',
-        detail:
-          'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.'
+          'Rock paper scissor game is very famous. Its very interesting game and it is written in vanilla javascript.',
+          link: 'https://rock-paper-secissor-26-04-2021-7de1c8.netlify.app/'
       }
     };
   
@@ -57,7 +52,7 @@ $(document).ready(function() {
     });
   
     var carousel = $('#carousel'),
-      slideWidth = 700,
+      slideWidth = 350,
       threshold = slideWidth / 3,
       dragStart,
       dragEnd;
